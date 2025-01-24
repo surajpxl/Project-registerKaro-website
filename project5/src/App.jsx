32d5031a769f4fb095383f2c0cbb7078d71299e8
Suraj Gupta <guptasuraj4455@gmail.com>
2025-01-25 00:24:34 +0530

    test react app

---
+++ project5/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h2 className='bg-gray-500'>Hello</h2>
+    </>
+  )
+}
+
+export default App
